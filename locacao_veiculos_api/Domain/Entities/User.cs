f45ceddeeb4eb2bfd2bdf1d47973da34d461f6f5
vianaerica <erica.viana01@gmail.com>
2021-02-10 20:01:03 -0300

    Adicionando atributos da classe User

--- locacao_veiculos_api/Domain/Entities/User.cs
+++ locacao_veiculos_api/Domain/Entities/User.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace locacao_veiculos_api.Domain.Entities
 {
     public class User
     {
-        
+        [Key]
+        public int id {get;set;}
+        [Required]
+        public string login {get;set;}
+        [Required]
+        public string password {get;set;}
     }
 }
