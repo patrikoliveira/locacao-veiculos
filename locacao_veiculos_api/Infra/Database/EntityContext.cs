6af6d6ae0919a68c9867346d596f932876fa259b
vianaerica <erica.viana01@gmail.com>
2021-02-10 20:18:11 -0300

    Criação de classe EntityContext

---
+++ locacao_veiculos_api/Infra/Database/EntityContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Newtonsoft.Json.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using locacao_veiculos_api.Domain.Entities;
+
+namespace locacao_veiculos_api.Infra.Database
+{
+    public class EntityContext : DbContext
+    {
+        
+    }
+}
