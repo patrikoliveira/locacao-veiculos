20778e18d639f61fdd8e65109df90c66f66ef247
vianaerica <erica.viana01@gmail.com>
2021-02-10 22:51:54 -0300

    Adicionando notações do EntityFramework na entidade User

--- locacao_veiculos_api/Domain/Entities/User.cs
+++ locacao_veiculos_api/Domain/Entities/User.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace locacao_veiculos_api.Domain.Entities
 {
+    [Table("users")]
     public class User
     {
+        [Key]
         public int id {get;set;}
+        [Required]
         public string login {get;set;}
+        [Required]
         public string password {get;set;}
         public UserType userType {get;set;}
     }
