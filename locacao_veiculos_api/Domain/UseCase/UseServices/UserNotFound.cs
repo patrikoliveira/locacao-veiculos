96fbce25aea87ae115eb93a55dda4f8ee53441af
vianaerica <erica.viana01@gmail.com>
2021-02-10 21:44:55 -0300

    Adicionadno notação de classe serializavel em UserNotFound

--- locacao_veiculos_api/Domain/UseCase/UseServices/UserNotFound.cs
+++ locacao_veiculos_api/Domain/UseCase/UseServices/UserNotFound.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace locacao_veiculos_api.Domain.UseCase.UseServices
 {
+    [Serializable]
     public class UserNotFound : Exception
     {
         public UserNotFound(string message) : base(message) { }
