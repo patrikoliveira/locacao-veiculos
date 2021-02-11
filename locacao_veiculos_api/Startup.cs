b35fae4cdf81c125f1faf46a8a76ac352adf1309
vianaerica <erica.viana01@gmail.com>
2021-02-10 22:39:48 -0300

    Adicionando autenticação e autorização no Startup

--- locacao_veiculos_api/Startup.cs
+++ locacao_veiculos_api/Startup.cs
@@ -1,16 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using locacao_veiculos_api.Domain.Entities;
+using locacao_veiculos_api.Infra.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
 
 namespace locacao_veiculos_api
 {
@@ -26,11 +30,48 @@ namespace locacao_veiculos_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            JToken jAppSettings = JToken.Parse(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "appsettings.json")));
+            services.AddDbContext<EntityContext>(options => options.UseNpgsql(jAppSettings["ConnectionString"].ToString()));
+
+            var key = Encoding.ASCII.GetBytes(jAppSettings["JwtToken"].ToString());
+            
+            services.AddControllersWithViews();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "locacao_veiculos_api", Version = "v1" });
+            });
+
+            services.AddMvc(config =>
+            {
+               var policy = new AuthorizationPolicyBuilder()
+                               .RequireAuthenticatedUser()
+                               .Build();
+               config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+           services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+               options.AddPolicy(UserType.Operador.ToString(), policy => policy.RequireClaim(UserType.Operador.ToString()));
+               options.AddPolicy(UserType.Cliente.ToString(), policy => policy.RequireClaim(UserType.Cliente.ToString()));
             });
         }
 
@@ -48,6 +89,13 @@ namespace locacao_veiculos_api
 
             app.UseRouting();
 
+             app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+            );
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
