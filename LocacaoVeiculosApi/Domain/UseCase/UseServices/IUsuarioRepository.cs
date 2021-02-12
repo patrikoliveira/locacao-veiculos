49f94fd28bfa4725f1d0c3e50ef59cd59c30fb52
vianaerica <erica.viana01@gmail.com>
2021-02-11 22:00:32 -0300

    Adicionando operações na interface de UsuarioRepository

--- LocacaoVeiculosApi/Domain/UseCase/UseServices/IUsuarioRepository.cs
+++ LocacaoVeiculosApi/Domain/UseCase/UseServices/IUsuarioRepository.cs
@@ -5,6 +5,11 @@ namespace LocacaoVeiculosApi.Domain.UseCase.UseServices
 {
     public interface IUsuarioRepository
     {
+        Task Update(User user);
+        Task Save(User user);
+        Task Delete(User user);
+        Task<ICollection<UserView>> All();
+        Task<User> FindById(int id);
         Task<Usuario> FindByLoginAndPassword(string login, string password);
     }
 }
