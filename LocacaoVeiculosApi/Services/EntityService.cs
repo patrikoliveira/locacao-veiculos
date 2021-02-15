e5f28aed95da44c0a812b7de407d767904686d27
vianaerica <erica.viana01@gmail.com>
2021-02-15 18:54:41 -0300

    Criação de service para Entity

---
+++ LocacaoVeiculosApi/Services/EntityService.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LocacaoVeiculosApi.Domain.Entities.Exceptions;
+using LocacaoVeiculosApi.Infrastructure.Repositories;
+
+namespace LocacaoVeiculosApi.Services
+{
+    public class EntityService
+    {
+        public EntityService(IEntityRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        private IEntityRepository repository;
+
+        public virtual async Task Save<T>(T entity)
+        {
+            await repository.Save(entity);
+        }
+
+        public async Task Delete<T>(int id)
+        {
+            if (id == 0) throw new UsuarioIdVazio("Id não pode ser vazio.");
+            var user = await repository.FindById<T>(id);
+            if (user == null) throw new UsuarioIdVazio("Registro não encontrado.");
+            await repository.Delete(user);
+        }
+
+        public async Task<ICollection<T>> All<T>()
+        {
+           return await repository.All<T>();
+        }
+    }
+}
