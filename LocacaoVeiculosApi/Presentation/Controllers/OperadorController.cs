8acdce39415b51afaa6401cb9c0e3a3c81c1474c
vianaerica <erica.viana01@gmail.com>
2021-02-15 19:53:48 -0300

    Implementando controller de Operador

--- LocacaoVeiculosApi/Presentation/Controllers/OperadorController.cs
+++ LocacaoVeiculosApi/Presentation/Controllers/OperadorController.cs
@@ -1,4 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LocacaoVeiculosApi.Domain.Entities;
+using LocacaoVeiculosApi.Domain.Entities.Enums;
+using LocacaoVeiculosApi.Domain.Entities.Exceptions;
+using LocacaoVeiculosApi.Infrastructure.Repositories;
+using LocacaoVeiculosApi.Presentation.ViewModel;
 using LocacaoVeiculosApi.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,11 +18,91 @@ namespace LocacaoVeiculosApi.Presentation.Controllers
     {
         private readonly UsuarioService _usuarioService;
         private readonly ILogger<OperadorController> _logger;
-        
+
         public OperadorController(ILogger<OperadorController> logger)
         {
             _logger = logger;
             _usuarioService = new UsuarioService(new UsuarioRepository(), new EntityRepository());
+        }
+
+        [HttpGet]
+        [Route("/operador")]
+        [Authorize(Roles = "Cliente, Operador")]
+        public async Task<ICollection<Operador>> Index()
+        {
+            return await _usuarioService.RetornaTodosUsuarioPorTipo<Operador>(TipoUsuario.Operador);
+        }
+
+        [HttpPost]
+        [Route("/operador")]
+        [Authorize(Roles = "Operador")]
+        public async Task<IActionResult> Create([FromBody] OperadorSalvar op)
+        {
+            var oper = EntityBuilder.Call<Operador>(op);
+            try
+            {
+                await _usuarioService.Save(oper);
+                return StatusCode(201);
+            }
+            catch (UsuarioUnico err)
+            {
+                return StatusCode(401, new
+                {
+                    Message = err.Message
+                });
+            }
+        }
+
+        private IActionResult StatusCode(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("/operador/{id}")]
+        [Authorize(Roles = "Operador")]
+        public async Task<IActionResult> Update(int id, [FromBody] OperadorSalvar op)
+        {
+            var oper = EntityBuilder.Call<Operador>(op);
+            oper.Id = id;
+            try
+            {
+                await _usuarioService.Save(oper);
+                return StatusCode(204);
+            }
+            catch (UsuarioUnico err)
+            {
+                return StatusCode(401, new
+                {
+                    Message = err.Message
+                });
+            }
+        }
+
+        [HttpDelete]
+        [Route("/operador/{id}")]
+        [Authorize(Roles = "Operador")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _usuarioService.Delete(id);
+                return StatusCode(204);
+            }
+            catch (UsuarioIdVazio err)
+            {
+                return StatusCode(401, new
+                {
+                    Message = err.Message
+                });
+            }
+            catch (UsuarioNotFound err)
+            {
+                return StatusCode(404, new
+                {
+                    Message = err.Message
+                });
+            }
         }
     }
 }
